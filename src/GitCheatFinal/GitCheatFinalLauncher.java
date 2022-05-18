4a35932887a9985866b33894bd712541188dcfb4
628HG09 <s.rutges@gmail.com>
2022-05-18 20:50:28 +0200

    GitLauncher toegevoefd

--- src/GitCheatFinal/GitCheatFinalLauncher.java
+++ src/GitCheatFinal/GitCheatFinalLauncher.java
@@ -3,5 +3,6 @@ package GitCheatFinal;
 public class GitCheatFinalLauncher {
     public static void main(String[] args) {
         System.out.println("git cheat");
+        System.out.println("aanpassing twee");
     }
 }
